84704c6ee67731bc357510bc173c89050781a4b3
Daniel Alvarez <91741699+DanielAlvarez2@users.noreply.github.com>
2024-06-24 19:20:19 -0400

    Initial commit

---
+++ controllers/home.js
@@ -0,0 +1,8 @@
+module.exports={
+    getIndex:(req,res)=>{
+        if(req.user){
+            return res.redirect('/feed')
+        }
+        res.render('index.ejs',{title:'HOME PAGE'})
+    }
+}
