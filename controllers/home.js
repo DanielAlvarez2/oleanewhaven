efc010dce3b4a2eba8433c445c81199451fbadfc
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-26 00:21:26 -0400

    index.ejs and specials.ejs functional

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,36 @@
+const Post = require('../models/Post')
+
 module.exports={
-    getIndex:(req,res)=>{
+    getIndex:async(req,res)=>{
         if(req.user){
             return res.redirect('/feed')
         }
-        res.render('index.ejs',{title:'HOME PAGE'})
+        const charcuterie = await Post.find({section:'charcuterie'}).sort({sequence:'asc'})
+        const appetizers = await Post.find({section:'appetizers'}).sort({sequence:'asc'})
+        const entrees = await Post.find({section:'entrees'}).sort({sequence:'asc'})
+        const sides = await Post.find({section:'sides'}).sort({sequence:'asc'})
+        res.render('index.ejs',{title:'HOME PAGE', 
+                                charcuterie:charcuterie, 
+                                appetizers:appetizers,
+                                entrees:entrees,
+                                sides:sides})
+    },
+    getSpecials: async(req,res)=>{
+        const appetizers = await Post.find({
+            $and: [
+                    {menu:'specials'},
+                    {section:'appetizers'},   
+                  ],
+                }).sort({sequence:'asc'})
+        const entrees = await Post.find({
+            $and: [
+                    {menu:'specials'},
+                    {section:'entrees'},
+                  ],
+                }).sort({sequence:'asc'})        
+        res.render('specials.ejs', 
+                    {title:'SPECIALS',
+                    appetizers:appetizers,
+                    entrees:entrees})
     }
 }
