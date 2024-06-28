f0ce8b4bc822dee332e04a48130fb09301fe03a1
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-28 12:07:19 -0400

    create Wine schema in models/Wine.js

--- models/Wine.js
+++ models/Wine.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose')
+
+const WineSchema = new mongoose.Schema({
+    grapes:{
+        type:String,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    vintage:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:String,
+        required:true,
+    }
+    })
